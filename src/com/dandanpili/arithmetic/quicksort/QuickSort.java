f762de0e33ed7241ba9a50463a6f16b30272e958
dandanpili <shaoruilin151@qq.com>
2021-03-19 18:30:40 +0800

    初始化项目

---
+++ src/com/dandanpili/arithmetic/quicksort/QuickSort.java
@@ -0,0 +1,8 @@
+package com.dandanpili.arithmetic.quicksort;
+
+/**
+ * @author shaoruilin
+ * @create 2021-03-19-18:23
+ */
+public class QuickSort {
+}
---
+++ src/com/dandanpili/com/dandanpili/design/singleton/EagerSingleton.java
@@ -0,0 +1,11 @@
+package com.dandanpili.com.dandanpili.design.singleton;
+
+/**
+ * @author shaoruilin
+ * @create 2021-03-19-18:28
+ */
+public class EagerSingleton {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
