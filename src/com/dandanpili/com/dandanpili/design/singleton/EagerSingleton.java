8671f50975fef5639f3ccf04b9720c781ca9912d
dandanpili <shaoruilin151@qq.com>
2021-03-21 14:00:49 +0800

    新增单例模式的饿汉式和懒汉式的双重检测机制

--- src/com/dandanpili/com/dandanpili/design/singleton/EagerSingleton.java
+++ src/com/dandanpili/com/dandanpili/design/singleton/EagerSingleton.java
@@ -4,8 +4,16 @@ package com.dandanpili.com.dandanpili.design.singleton;
  * @author shaoruilin
  * @create 2021-03-19-18:28
  */
+//单例模式的饿汉式
 public class EagerSingleton {
-    public static void main(String[] args) {
-        System.out.println("hello world");
+    //私有化类的构造器，让外部无法使用
+    private EagerSingleton() {
+
+    }
+
+    private static EagerSingleton eagerSingleton = new EagerSingleton();
+
+    public static EagerSingleton getInstance() {
+        return eagerSingleton;
     }
 }
---
+++ src/com/dandanpili/com/dandanpili/design/singleton/LazySingleton.java
@@ -0,0 +1,29 @@
+package com.dandanpili.com.dandanpili.design.singleton;
+
+/**
+ * @author shaoruilin
+ * @create 2021-03-21-13:43
+ */
+//单例模式的懒汉式双重检测模式
+public class LazySingleton {
+    //volatile 保证可见性和禁止指令重排序
+    private static volatile LazySingleton lazySingleton;
+
+    private LazySingleton() {
+
+    }
+    public static LazySingleton getInstance() {
+        //第一次检查
+        if (lazySingleton == null) {
+            //同步代码块
+            synchronized(LazySingleton.class) {
+                //第二次检查
+                if (lazySingleton == null) {
+                    //对象实例化是一个非原子性操作
+                    lazySingleton = new LazySingleton();
+                }
+            }
+        }
+        return lazySingleton;
+    }
+}
